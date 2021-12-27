45401e167715cbea56b4aad99b0a9c4cd69dda07
chm209 <changmin7537@gmail.com>
2021-12-27 17:01:48 +0900

    add: UI 뼈대 구성

--- UTube_downloader_GUI/MainWindow.xaml.cs
+++ UTube_downloader_GUI/MainWindow.xaml.cs
@@ -24,5 +24,35 @@ namespace UTube_downloader_GUI
         {
             InitializeComponent();
         }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            this.DragMove();
+        }
+
+        private void pinned_button_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.Topmost != true)
+            {
+                this.Topmost = true;
+                pinned_button.Content = "\xE77A";
+            }
+            else
+            {
+                this.Topmost = false;
+                pinned_button.Content = "\xE718";
+            }
+        }
+
+        private void minimize_button_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void close_button_Click(object sender, RoutedEventArgs e)
+        {
+            GetWindow(this).Close();
+        }
     }
 }
